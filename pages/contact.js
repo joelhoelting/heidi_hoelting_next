2226fdd5c65f560c7c56c9bb31e53a06cdbd1373
Joel Hoelting <joelhoelting@protonmail.com>
2019-05-05 23:48:05 -0400

    Update contact form submitter to work with a more reusable API email send endpoint

--- pages/contact.js
+++ pages/contact.js
@@ -4,6 +4,96 @@ import styled from 'styled-components';
 import { validEmail } from '../helpers/strings';
 import { mediaMin } from '../styles/mediaQueries';
 
+const ContactWrapper = styled.div`
+  height: 100%;
+  text-align: center;
+  width: 100%;
+  ${mediaMin.tablet`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  `}
+  h1 {
+    margin: 0;
+  }
+  form {
+    margin: 0 auto;
+    max-width: 90%;
+    width: 600px;
+    label {
+      display: block;
+      height: 70px;
+      position: relative;
+      margin: 1em 0;
+      width: 100%;
+      input {
+        background: none;
+        border-left: none;
+        border-top: none;
+        border-right: none;
+        color: #000;
+        font-size: 1rem;
+        height: 40px;
+        padding: 0 1em;
+        width: 100%;
+
+        &#email {
+          border-bottom: ${props => (props.errors.emailValid ? '2px solid #000' : '2px solid red')};
+        }
+        &#name {
+          border-bottom: ${props => (props.errors.nameValid ? '2px solid #000' : '2px solid red')};
+        }
+        &.filled + span,
+        &:focus + span {
+          top: 3.2em;
+          font-size: 0.8rem;
+          color: grey;
+        }
+      }
+      span {
+        cursor: text;
+        font-size: 1rem;
+        left: 1em;
+        position: absolute;
+        top: 0.8em;
+        transition: all 200ms ease;
+      }
+    }
+    textarea {
+      background: none;
+      border: ${props => (props.errors.textareaValid ? '2px solid #000' : '2px solid red')};
+      border-radius: 5px;
+      font-size: 1rem;
+      height: 200px;
+      margin: 1em 0;
+      padding: 1em 1em;
+      resize: none;
+      width: 100%;
+    }
+    button {
+      background: none;
+      border: 2px solid #000;
+      border-radius: 5px;
+      color: #000;
+      cursor: pointer;
+      font-size: 1.4rem;
+      margin: 1em 0;
+      padding: 1em 1em;
+      width: 100%;
+      transition: all 300ms ease;
+      &:hover {
+        background: #000;
+        color: #fff;
+      }
+      &:focus {
+        background: #000;
+        color: #fff;
+      }
+    }
+  }
+`;
+
 class Contact extends Component {
   constructor(props) {
     super(props);
@@ -27,21 +117,40 @@ class Contact extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    this.validateForm();
+    if (this.isFormValid()) {
+      const { email, name, textarea } = this.state;
+
+      const emailBody = {
+        to: 'joelhoelting@protonmail.com',
+        subject: `HeidiHoelting.com: ${name}`,
+        from: 'HeidiHoelting.com',
+        message: `Heidi, you have a new email from: ${email}\n\n${textarea}`
+      }
+      
+      fetch('http://localhost:3001/email/send', {
+        method: 'POST',
+        body: JSON.stringify(emailBody), // data can be `string` or {object}!
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res => console.log('Form Submitted Successfully', res))
+        .catch(error => console.error('Error:', error));
+    }
   }
 
-  validateForm() {
+  isFormValid() {
     const { name, email, textarea } = this.state;
 
     let nameValid, emailValid, textareaValid;
-    let invalidFieldsPresent = false;
+    let isFormValid = true;
 
-    nameValid = name.length > 4 ? true : false;
-    emailValid = validEmail(email) ? true : false;
-    textareaValid = textarea.length > 5 ? true : false;
+    nameValid = name.length > 2;
+    emailValid = validEmail(email);
+    textareaValid = textarea.length > 5;
 
     if (!nameValid || !emailValid || !textareaValid) {
-      invalidFieldsPresent = true;
+      isFormValid = false;
     }
 
     this.setState({
@@ -52,7 +161,7 @@ class Contact extends Component {
       }
     });
 
-    return invalidFieldsPresent;
+    return isFormValid;
   }
 
   render() {
@@ -98,90 +207,3 @@ class Contact extends Component {
 }
 
 export default Contact;
-
-const ContactWrapper = styled.div`
-  height: 100%;
-  width: 100%;
-  text-align: center;
-  ${mediaMin.tablet`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-  `}
-  h1 {
-    margin: 0;
-  }
-  form {
-    margin: 0 auto;
-    max-width: 90%;
-    width: 600px;
-    label {
-      display: block;
-      position: relative;
-      height: 70px;
-      margin: 1em 0;
-      width: 100%;
-      input {
-        background: none;
-        border-top: none;
-        border-right: none;
-        border-left: none;
-        height: 40px;
-        padding: 0 1em;
-        width: 100%;
-        color: #000;
-        &#email {
-          border-bottom: ${props => (props.errors.emailValid ? '2px solid #000' : '2px solid red')};
-        }
-        &#name {
-          border-bottom: ${props => (props.errors.nameValid ? '2px solid #000' : '2px solid red')};
-        }
-        &.filled + span,
-        &:focus + span {
-          top: 3.2em;
-          font-size: 0.8rem;
-          color: grey;
-        }
-      }
-      span {
-        cursor: text;
-        font-size: 1rem;
-        left: 1em;
-        position: absolute;
-        top: 0.8em;
-        transition: all 200ms ease;
-      }
-    }
-    textarea {
-      background: none;
-      border: ${props => (props.errors.textareaValid ? '2px solid #000' : '2px solid red')};
-      border-radius: 5px;
-      height: 200px;
-      margin: 1em 0;
-      padding: 1em 1em;
-      resize: none;
-      width: 100%;
-    }
-    button {
-      background: none;
-      border: 2px solid #000;
-      border-radius: 5px;
-      color: #000;
-      cursor: pointer;
-      font-size: 1.4rem;
-      margin: 1em 0;
-      padding: 1em 1em;
-      width: 100%;
-      transition: all 300ms ease;
-      &:hover {
-        background: #000;
-        color: #fff;
-      }
-      &:focus {
-        background: #000;
-        color: #fff;
-      }
-    }
-  }
-`;
