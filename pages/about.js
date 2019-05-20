79e772e1fdfcf38ec83ce382c6cc12538e7a402a
Joel Hoelting <joelhoelting@protonmail.com>
2019-05-19 22:55:16 -0400

    Add instagram logo and mobile about page background image

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,35 @@
 import { Component } from 'react';
 import styled from 'styled-components';
+import { mediaMin } from '../styles/mediaQueries';
 
 const AboutWrapper = styled.div`
   height: 100%;
   width: 100%;
-  background-image: url('/static/images/pages/about/heidi_about_background.jpg');
+  background-image: url('/static/images/pages/about/heidi_about_background_mobile.jpg');
   background-size: cover;
   background-position: center center;
   opacity: ${props => (!props.mounted ? 0 : 1)};
   transition: opacity 300ms ease;
   position: relative;
+  ${mediaMin.tablet`
+    background-image: url('/static/images/pages/about/heidi_about_background.jpg');
+  `}
+  .mobile-overlay {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    ${mediaMin.tablet`
+      display: none;
+    `}
+  }
   section {
     position: absolute;
     right: 0;
     height: 100%;
-    width: 60%;
+    width: auto;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -22,12 +37,28 @@ const AboutWrapper = styled.div`
     color: #fff;
     transition: opacity 400ms ease-in-out 200ms;
     opacity: ${props => (!props.mounted ? 0 : 1)};
+    ${mediaMin.tablet`
+      width: 60%;
+    `}
     h1 {
-      font-size: 3rem;
+      font-size: 2rem;
+      ${mediaMin.tablet`
+        font-size: 3rem;
+      `}
     }
     p {
-      max-width: 750px;
+      width: 90%;
+      ${mediaMin.tablet`
+        max-width: 750px;
+      `}
     }
+  }
+  img.instagram-logo {
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    width: 40px;
+    cursor: pointer;
   }
 `;
 
@@ -50,6 +81,7 @@ class About extends Component {
     const { mounted } = this.state;
     return (
       <AboutWrapper mounted={mounted}>
+        <div className="mobile-overlay" />
         <section>
           <h1>Heidi Hoelting</h1>
           <p>
@@ -59,6 +91,9 @@ class About extends Component {
             deserunt exercitation. Velit officia enim labore amet nisi esse reprehenderit ad.
           </p>
         </section>
+        <a href="https://www.instagram.com/heidi_c_nyc/">
+          <img className="instagram-logo" src="/static/images/logos/instagram.svg" alt="instagram_logo" />
+        </a>
       </AboutWrapper>
     );
   }
