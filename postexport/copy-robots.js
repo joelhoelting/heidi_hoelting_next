d1d00a23c249c35ec794670409c1c96fa1dae586
Joel Hoelting <joelhoelting@protonmail.com>
2019-12-08 22:51:30 -0500

    Removes another eslint warning

--- postexport/copy-robots.js
+++ postexport/copy-robots.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 exports.copyRobots = () => {
   fs.copyFile('./postexport/robots.txt', './out/robots.txt', err => {
     if (err) throw err;
+    // eslint-disable-next-line
     console.log('Robots.txt was copied to destination');
   });
 };
